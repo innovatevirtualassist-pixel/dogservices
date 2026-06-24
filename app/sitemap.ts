047c1efdcf65254d38ca2611f878e2707d5c8559
hellosigmainteriors <hellosigmainteriors@gmail.com>
2026-06-24 10:06:25 +0100

    Add recent walks and local walks to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,18 +4,37 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.penkridgedogservices.co.uk/",
+      lastModified: new Date(),
     },
+
     {
       url: "https://www.penkridgedogservices.co.uk/dog-walking-penkridge",
+      lastModified: new Date(),
     },
+
     {
       url: "https://www.penkridgedogservices.co.uk/dog-boarding-penkridge",
+      lastModified: new Date(),
     },
+
     {
       url: "https://www.penkridgedogservices.co.uk/dog-sitting-penkridge",
+      lastModified: new Date(),
     },
+
     {
       url: "https://www.penkridgedogservices.co.uk/contact",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.penkridgedogservices.co.uk/recent-walks",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.penkridgedogservices.co.uk/local-dog-walks",
+      lastModified: new Date(),
     },
   ]
 }
