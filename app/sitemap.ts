01d78001a9c969af469d5e693e99600058b51f3a
Maria Clausen <innovatevirtualassist@gmail.com>
2026-08-28 06:33:43 +0200

    Add new URL to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -113,6 +113,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
   lastModified: new Date(),
   changeFrequency: "monthly",
   priority: 0.7,
+},
+    {
+  url: "https://www.penkridgedogservices.co.uk/hu-norcenni-girasole",
+  lastModified: new Date(),
+  changeFrequency: "monthly",
+  priority: 0.7,
 }
   ]
 }
